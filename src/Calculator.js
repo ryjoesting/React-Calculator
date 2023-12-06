a60cefbc10594796bd3e913fba0a17a21a885b25
Ryan Joesting <nsd-rjoesting@nucor.com>
2023-12-06 12:32:16 -0600

    Styled the calculator aethetic

--- src/Calculator.js
+++ src/Calculator.js
@@ -7,22 +7,23 @@ function Calculator() {
         <div className='calculator'>
             <Screen />
             <div className='grid'>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
-                <button className='btn'>#</button>
+                <button className='btn btn-wide btn-AC'>AC</button>
+                <button className='btn btn-bold'>/</button>
+                <button className='btn btn-bold'>X</button>
+                <button className='btn'>7</button>
+                <button className='btn'>8</button>
+                <button className='btn'>9</button>
+                <button className='btn btn-bold'>-</button>
+                <button className='btn'>4</button>
+                <button className='btn'>5</button>
+                <button className='btn'>6</button>
+                <button className='btn btn-bold'>+</button>
+                <button className='btn'>1</button>
+                <button className='btn'>2</button>
+                <button className='btn'>3</button>
+                <button className='btn btn-tall btn-bold'>=</button>
+                <button className='btn btn-wide'>0</button>
+                <button className='btn' style={{fontSize: '36px'}}>.</button>
             </div>
         </div>
     );
